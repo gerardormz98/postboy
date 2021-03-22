e4d72495836a509a44bcfd9a288df5890f6ebe21
Gerardo Ramírez <gerardo.rmz98@hotmail.com>
2021-03-22 02:29:25 -0600

    Change footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
         <div className="footer bg-dark">
             <Container className="h-100 d-flex align-items-center justify-content-between">
                 <span className="text-white-50 brand">Postboy</span>
-                <span className="text-white-50">Built by Gerardo Ramírez</span>
+                <span className="text-white-50">Made with <FontAwesomeIcon icon={faHeart} /> by Gerardo Ramírez</span>
             </Container>
         </div>
     );
