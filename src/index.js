ab486c79749d641d2ebac77a5e235b8bbde6243a
Gerardo Ramírez <gerardo.rmz98@hotmail.com>
2021-03-23 12:07:23 -0600

    Change scrollbar styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import AppRouter from "./router/AppRouter";
 import './styles/styles.scss';
 
-// TODO: Always show scroll bar, change style
 // TODO: Tab overflow scroll.
 
 // TODO: Add history bar feature.
